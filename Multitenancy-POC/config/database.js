456f6e76127832c3253533648a0d5a56034923c0
cod3kid <djsufail@gmail.com>
2021-10-21 06:50:43 +0530

    Knexfile added

--- Multitenancy-POC/config/database.js
+++ Multitenancy-POC/config/database.js
@@ -1,8 +1,14 @@
-// const Sequelize = require("sequelize");
+const knex = require("knex");
+const knexfile = require("./knexfile");
 
-// const sequelize = new Sequelize("multitenancy", "postgres", "postgres", {
-//   dialect: "postgres",
-//   host: "localhost",
-// });
+// TODO: in prod, don't hardcode db config here
+// but instead pull values in via env vars or
+// even better with a config library like
+// convict
 
-// module.exports = sequelize;
+// TODO in prod, use dependency injection
+// to create knex instance so db access can be mocked
+// for tests
+
+const db = knex(knexfile.development);
+module.exports = db;
