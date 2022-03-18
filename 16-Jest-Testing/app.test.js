6a071e06605449ff1a70caeb0f6912c64f64ba7b
cod3kid <djsufail@gmail.com>
2022-03-18 23:31:19 +0530

    More Unit Tests

--- 16-Jest-Testing/app.test.js
+++ 16-Jest-Testing/app.test.js
@@ -1,3 +1,11 @@
 test("Our First Test", () => {
   expect(3 + 2).toBe(5);
 });
+
+test("Greater Than", () => {
+  expect(2 + 2).toBeGreaterThan(3);
+});
+
+test("Null", () => {
+  expect(null).toBeNull();
+});
