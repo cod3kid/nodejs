33b7e9e67c30fe6343ab77136d1be639e1b24031
cod3kid <djsufail@gmail.com>
2022-03-19 23:40:38 +0530

    More matchers

--- 16-Jest-Testing/app.test.js
+++ 16-Jest-Testing/app.test.js
@@ -1,11 +1,22 @@
-test("Our First Test", () => {
+test("First Test", () => {
   expect(3 + 2).toBe(5);
 });
 
-test("Greater Than", () => {
-  expect(2 + 2).toBeGreaterThan(3);
+test("4 is greater than 2", () => {
+  expect(2 + 2).toBeGreaterThan(2);
 });
 
-test("Null", () => {
-  expect(null).toBeNull();
+test("variable num is null", () => {
+  const num = null;
+  expect(num).toBeNull();
+});
+
+test("check if ending contains ing", () => {
+  expect("ending").toMatch(/ing/);
+});
+
+test("check for array equality", () => {
+  const data = ["Naruto", "Sasuke"];
+  data.push("Itachi");
+  expect(data).toEqual(["Naruto", "Sasuke", "Itachi"]);
 });
